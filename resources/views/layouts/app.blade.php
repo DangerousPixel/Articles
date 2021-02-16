4662ff7abe5724bae21a7a95d65a4e5b5b91763b
DanPixel <siidrove@gmail.com>
2021-02-16 23:13:05 +0300

    added profile to dropdown list

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -55,12 +55,12 @@
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="{{route('profile.show', Auth::user()->id)}}" > Profile </a>
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
                                         {{ __('Logout') }}
                                     </a>
-
                                     <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
                                         @csrf
                                     </form>
@@ -68,6 +68,7 @@
                             </li>
                         @endguest
                     </ul>
+
                 </div>
             </div>
         </nav>
